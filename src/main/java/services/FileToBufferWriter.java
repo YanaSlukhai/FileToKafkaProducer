0efb7bf4ded39924477ee81f68855605dbfd8da2
Yana_Slukhai <Yana_Slukhai@epam.com>
2018-11-02 18:01:55 +0200

    working on sandbox vrsn

---
+++ src/main/java/services/FileToBufferWriter.java
@@ -0,0 +1,34 @@
+package services;
+
+import model.FileEntriesBuffer;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class FileToBufferWriter {
+
+    public void readFileFromFileToBuffer(String filePath, FileEntriesBuffer buffer) {
+
+        try
+        {
+            File f = new File(filePath);
+            BufferedReader b = new BufferedReader(new FileReader(f));
+            String readLine = "";
+
+            System.out.println("Reading file using Buffered Reader");
+            while ((readLine = b.readLine()) != null) {
+                System.out.println("Reading from file " + readLine);
+                buffer.getBuffer().put(readLine);
+            }
+            buffer.setAllTheFileWasStreamedToBuffer(true);
+
+        } catch( IOException | InterruptedException e)
+        {
+            e.printStackTrace();
+        }
+}
+
+
+}
